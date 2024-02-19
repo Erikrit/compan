c9e1fdf5ccf5dabf0a35a91f8096edaa8d23d6a0
CarolHaberland <carolhaberland@gmail.com>
2024-02-19 17:05:07 -0300

    atualizando layout da aba anuncio

---
+++ compan/pages/Anuncio/Verificação Anuncio/verificacaoAnuncio.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./verificacao.module.css";
+
+export const VerificacaoAnuncio = ({
+  categoria,
+  estado,
+  cidade,
+  cep,
+  endereco,
+  nome,
+  descricao,
+}) => {
+  return (
+    <div className={styles.inserir_dados_container}>
+      <div className={styles.contaner_dados}>
+        <div className={styles.cabecalho_container}>
+          <h3 className={styles.cabecalho}>Resumo do Anúncio</h3>
+        </div>
+        <div className={styles.resumo_info}>
+        <p>
+          <strong>Categoria:</strong> {categoria}
+        </p>
+        <p>
+          <strong>Estado:</strong> {estado}
+        </p>
+        <p>
+          <strong>Cidade:</strong> {cidade}
+        </p>
+        <p>
+          <strong>CEP:</strong> {cep}
+        </p>
+        <p>
+          <strong>Endereço:</strong> {endereco}
+        </p>
+        <p>
+          <strong>Título do Anúncio:</strong> {nome}
+        </p>
+        <p>
+          <strong>Descrição:</strong> {descricao}
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- compan/pages/Anuncio/anuncio.js
+++ compan/pages/Anuncio/anuncio.js
@@ -1,26 +1,76 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./anuncio.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import estadosCidades from "../../src/companents/Assets/estados-cidades.json";
+import { useRef } from "react";
+import { VerificacaoAnuncio } from "./Verificação Anuncio/verificacaoAnuncio";
+
+const categorias = {
+  Eletronicos: ["Celular", "Computador", "Notebook", "Headset"],
+  Roupas: ["Camiseta", "Calça", "Sapato"],
+  Automoveis: ["Carro", "Moto", "Caminhao"],
+  Acompanhantes: ["Homem", "Mulher", "Trans"],
+  Casas: ["Apartamento", "Casa", "Terreno"],
+};
 
 const Anuncio = () => {
-  const [nome, setNome] = useState("");
-  const [descricao, setDescricao] = useState("");
-  const [valor, setValor] = useState("");
-  const [cidade, setCidade] = useState("");
-  const [estado, setEstado] = useState("");
-  const [imagens, setImagens] = useState([]);
+  const [abaAtiva, setAbaAtiva] = useState(1);
+  const [estadoSelecionado, setEstadoSelecionado] = useState("");
+  const [cidadeSelecionada, setCidadeSelecionada] = useState("");
+  const [estados, setEstados] = useState([]);
+  const [cidades, setCidades] = useState([]);
+  const [categoria, setCategoria] = useState("");
   const [cep, setCep] = useState("");
   const [endereco, setEndereco] = useState("");
+  const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [totalAbas, setTotalAbas] = useState(3);
+  const inputImagensRef = useRef(null);
 
-  const handleNomeChange = (event) => {
-    setNome(event.target.value);
+  useEffect(() => {
+    if (estadoSelecionado) {
+      const estadoData = estadosCidades.estados.find(
+        (estado) => estado.nome === estadoSelecionado
+      );
+      if (estadoData) {
+        setCidades(estadoData.cidades);
+      } else {
+        setCidades([]);
+      }
+    } else {
+      setCidades([]);
+    }
+  }, [estadoSelecionado]);
+
+  useEffect(() => {
+    setTotalAbas(3);
+  }, []);
+
+  const handleAbaClick = (aba) => {
+    setAbaAtiva(aba);
   };
 
-  const handleDescricaoChange = (event) => {
-    setDescricao(event.target.value);
+  const handleEstadoChange = (event) => {
+    const estadoSelecionado = event.target.value;
+    setEstadoSelecionado(estadoSelecionado);
+
+    const estadoData = estadosCidades.estados.find(
+      (estado) => estado.Nome === estadoSelecionado
+    );
+
+    if (estadoData) {
+      setCidades(estadoData.Cidades);
+    } else {
+      setCidades([]);
+    }
   };
 
-  const handleValorChange = (event) => {
-    setValor(event.target.value);
+  const handleCidadeChange = (event) => {
+    setCidadeSelecionada(event.target.value);
+  };
+
+  const handleCategoriaChange = (event) => {
+    setCategoria(event.target.value);
   };
 
   const handleCepChange = (event) => {
@@ -31,108 +81,272 @@ const Anuncio = () => {
     setEndereco(event.target.value);
   };
 
-  const handleCidadeChange = (event) => {
-    setCidade(event.target.value);
+  const handleNomeChange = (event) => {
+    setNome(event.target.value);
   };
 
-  const handleEstadoChange = (event) => {
-    setEstado(event.target.value);
+  const handleDescricaoChange = (event) => {
+    setDescricao(event.target.value);
   };
 
   const handleImagensChange = (event) => {
     const files = Array.from(event.target.files);
+    if (files.length > 6) {
+      files.splice(6);
+    }
     setImagens(files);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
     console.log({
+      categoria,
+      estado: estadoSelecionado,
+      cidade: cidadeSelecionada,
+      cep,
+      endereco,
       nome,
       descricao,
-      valor,
-      cidade,
-      estado,
-      imagens,
     });
+  };
+
+  const handleLabelClick = () => {
+    inputImagensRef.current.click();
+  };
+
+  const handleNextButtonClick = () => {
+    setAbaAtiva(abaAtiva + 1);
+  };
+
+  const handlePublicarClick = () => {
+    // FAZER A LOGICA
+    console.log("Anúncio publicado!");
   };
 
   return (
     <div className={styles.anuncio_container}>
       <div className={styles.anuncio_container2}>
         <h2>Criar Anúncio</h2>
-        <form onSubmit={handleSubmit} className={styles.form_container}>
-          <div className={styles.input_group}>
-            <label>
-              *Anúncio:
-              <input type="text" value={nome} onChange={handleNomeChange} />
-            </label>
+        <div className={styles.abas_container}>
+          <div
+            className={`${styles.aba} ${
+              abaAtiva === 1 ? styles.aba_ativa : ""
+            }`}
+            onClick={() => handleAbaClick(1)}
+          >
+            Inserir Dados
           </div>
-          <br />
-          <div className={styles.input_group}>
-            <label>
-              *Descrição:
-              <textarea
-                value={descricao}
-                onChange={handleDescricaoChange}
-                className={styles.descricao_textarea}
-              />
-            </label>
+          <div
+            className={`${styles.aba} ${
+              abaAtiva === 2 ? styles.aba_ativa : ""
+            }`}
+            onClick={() => handleAbaClick(2)}
+          >
+            Promover
           </div>
-          <br />
-          <div className={styles.input_group}>
-            <label>
-              *Valor:
-              <input type="text" value={valor} onChange={handleValorChange} />
-            </label>
+          <div
+            className={`${styles.aba} ${
+              abaAtiva === 3 ? styles.aba_ativa : ""
+            }`}
+            onClick={() => handleAbaClick(3)}
+          >
+            Publicar
           </div>
-          <br />
-          <div className={styles.input_group}>
-            <label>
-              *Cidade:
-              <input type="text" value={cidade} onChange={handleCidadeChange} />
-            </label>
-            <br />
-            <label>
-              *Estado:
-              <input type="text" value={estado} onChange={handleEstadoChange} />
-            </label>
+        </div>
+        {abaAtiva === 1 && (
+          <div className={styles.inserir_dados_container}>
+            <div className={styles.contaner_dados}>
+              <div className={styles.cabecalho_container}>
+                <h3 className={styles.cabecalho}>Inserir Dados do Anúncio</h3>
+                <p className={styles.obrigatorio}>*Campos obrigatórios</p>
+              </div>
+              <div className={styles.input_group}>
+                <label htmlFor="select_categoria">*Selecionar Categoria:</label>
+                <select
+                  id="select_categoria"
+                  onChange={handleCategoriaChange}
+                  value={categoria}
+                  className={styles.select_categoria}
+                >
+                  <option value="">Selecione...</option>
+                  {Object.keys(categorias).map((categoriaKey) => (
+                    <optgroup label={categoriaKey} key={categoriaKey}>
+                      {categorias[categoriaKey].map((item) => (
+                        <option value={item} key={item}>
+                          {item}
+                        </option>
+                      ))}
+                    </optgroup>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.input_group}>
+                <label htmlFor="select_estado">*Selecionar Estado:</label>
+                <select
+                  id="select_estado"
+                  onChange={handleEstadoChange}
+                  value={estadoSelecionado}
+                  className={styles.select_estado}
+                >
+                  <option value="">Selecione...</option>
+                  {estadosCidades.estados.map((estado) => (
+                    <option key={estado.sigla} value={estado.nome}>
+                      {estado.nome}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.input_group}>
+                <label htmlFor="select_cidade">*Selecionar Cidade:</label>
+                <select
+                  id="select_cidade"
+                  onChange={handleCidadeChange}
+                  value={cidadeSelecionada}
+                  className={styles.select_cidade}
+                >
+                  <option value="">Selecione...</option>
+                  {cidades.map((cidade, index) => (
+                    <option key={index} value={cidade}>
+                      {cidade}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.input_endereco}>
+                <div className={styles.input_group}>
+                  <label htmlFor="input_cep">CEP:</label>
+                  <input
+                    type="text"
+                    id="input_cep"
+                    value={cep}
+                    onChange={handleCepChange}
+                    className={styles.input_CEP_text}
+                  />
+                </div>
+                <div className={styles.input_group}>
+                  <label htmlFor="input_endereco">Endereço:</label>
+                  <input
+                    type="text"
+                    id="input_endereco"
+                    value={endereco}
+                    onChange={handleEnderecoChange}
+                    className={styles.input_endereco_text}
+                  />
+                </div>
+              </div>
+              <div className={styles.input_group}>
+                <label htmlFor="input_nome">*Título do Anúncio:</label>
+                <input
+                  type="text"
+                  id="input_nome"
+                  value={nome}
+                  onChange={handleNomeChange}
+                  className={styles.input_text}
+                />
+              </div>
+              <div className={styles.input_group}>
+                <label htmlFor="textarea_descricao">*Descrição:</label>
+                <textarea
+                  id="textarea_descricao"
+                  value={descricao}
+                  onChange={handleDescricaoChange}
+                  className={styles.input_descricao_text}
+                />
+              </div>
+              <div className={styles.contaner_dados}>
+                <div className={styles.cabecalho_container2}>
+                  <h3 className={styles.cabecalho}>Imagens</h3>
+                </div>
+                <div className={`${styles.conteudo_aba}`}>
+                  <div className={styles.input_group}>
+                    <p>Insira até 6 imagens.</p>
+                    <label
+                      htmlFor="input_imagens"
+                      className={styles.input_imagens_label}
+                      onClick={handleLabelClick}
+                    >
+                      Imagens
+                    </label>
+                    <input
+                      id="input_imagens"
+                      type="file"
+                      accept="image/*"
+                      multiple
+                      onChange={handleImagensChange}
+                      className={styles.input_imagens}
+                      ref={inputImagensRef}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.button_group}>
+                {abaAtiva < totalAbas && (
+                  <button
+                    type="button"
+                    className={styles.next_button}
+                    onClick={handleNextButtonClick}
+                  >
+                    Próximo
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
-          <div className={styles.input_group}>
-            <label>
-              CEP:
-              <input type="text" value={cep} onChange={handleCepChange} />
-            </label>
-            <br />
-            <label>
-              Endereço:
-              <input
-                type="text"
-                value={endereco}
-                onChange={handleEnderecoChange}
-              />
-            </label>
+        )}
+        {abaAtiva === 2 && (
+          <div className={`${styles.inserir_dados_container}`}>
+            <div className={styles.contaner_dados}>
+              <div className={styles.cabecalho_container}>
+                <h3 className={styles.cabecalho}>Promova seu anúncio</h3>
+              </div>
+              <div className={styles.info_ofertas}>
+                <h6>1. SELECIONE A OFERTA</h6>
+                <p>Promova seu anúncio ou continue e Publique Grátis</p>
+              </div>
+              {/* Conteúdo adicional da aba Promover */}
+              <div className={styles.info_buttons}>
+                {abaAtiva < totalAbas && (
+                  <button
+                    type="button"
+                    className={styles.gratis_button}
+                    onClick={handleNextButtonClick}
+                  >
+                    Publique Grátis
+                  </button>
+                )}
+                {abaAtiva < totalAbas && (
+                  <button
+                    type="button"
+                    className={styles.promova_button}
+                    onClick={handleNextButtonClick}
+                  >
+                    Publique e Promova
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
-          <br />
-          <div className={styles.input_group}>
-            <label htmlFor="input_imagens" className={styles.label_imagens}>
-              *Escolha as imagens
-            </label>
-            <input
-              id="input_imagens"
-              type="file"
-              multiple
-              onChange={handleImagensChange}
-              className={styles.input_imagens}
+        )}
+        {abaAtiva === 3 && (
+          <div className={`${styles.conteudo_aba}`}>
+            <VerificacaoAnuncio
+              categoria={categoria}
+              estado={estadoSelecionado}
+              cidade={cidadeSelecionada}
+              cep={cep}
+              endereco={endereco}
+              nome={nome}
+              descricao={descricao}
             />
-          </div>
-          <br />
-          <div className={styles.button_group}>
-            <button type="submit" className={styles.submit_button}>
-              Enviar Anúncio
+            <button
+              type="submit"
+              className={`${styles.submit_button} ${styles.botao_publicar}`}
+              onClick={handlePublicarClick}
+            >
+              Publicar
             </button>
           </div>
-        </form>
+        )}
       </div>
     </div>
   );
